cd95f50e36132e520c939ec9310876db29d9e583
cwahlfeldt <chriswahlfeldt@gmail.com>
2025-09-21 10:24:31 -0500

    update to godot 4.5

--- src/Game/GameManager.cs
+++ src/Game/GameManager.cs
@@ -3,42 +3,42 @@ using Godot;
 
 namespace Game
 {
-    public partial class GameManager : Node3D
-    {
-        private Systems _systems;
+	public partial class GameManager : Node3D
+	{
+		private Systems _systems;
 
-        public override void _Ready()
-        {
-            Events.Instance.TurnChanged += OnTurnChanged;
+		public override void _Ready()
+		{
+			Events.Instance.TurnChanged += OnTurnChanged;
 
-            _systems = new Systems(this);
+			_systems = new Systems(this);
 
-            var entityManager = _systems.GetEntityManager();
+			var entityManager = _systems.GetEntityManager();
 
-            _systems.RegisterConcurrent<ComponentDebugSystem>();
-            _systems.RegisterConcurrent<DebugSystem>();
-            _systems.RegisterConcurrent<TileHighlightSystem>();
+			_systems.RegisterConcurrent<ComponentDebugSystem>();
+			_systems.RegisterConcurrent<DebugSystem>();
+			_systems.RegisterConcurrent<TileHighlightSystem>();
 
-            _systems.Register<RenderSystem>();
-            _systems.Register<TurnSystem>();
-            _systems.Register<PlayerSystem>();
-            _systems.Register<EnemySystem>();
-            _systems.Register<RangeSystem>();
-            _systems.Register<MovementSystem>();
-            _systems.Register<CombatSystem>();
+			_systems.Register<RenderSystem>();
+			_systems.Register<TurnSystem>();
+			_systems.Register<PlayerSystem>();
+			_systems.Register<EnemySystem>();
+			_systems.Register<RangeSystem>();
+			_systems.Register<MovementSystem>();
+			_systems.Register<CombatSystem>();
 
-            entityManager.CreateGrid(5);
-            entityManager.CreatePlayer();
-            entityManager.CreateEnemy(UnitType.Grunt);
-            entityManager.CreateEnemy(UnitType.Grunt);
-            entityManager.CreateEnemy(UnitType.Grunt);
+			entityManager.CreateGrid(5);
+			entityManager.CreatePlayer();
+			entityManager.CreateEnemy(UnitType.Grunt);
+			entityManager.CreateEnemy(UnitType.Grunt);
+			entityManager.CreateEnemy(UnitType.Grunt);
 
-            _systems.Initialize();
-        }
+			_systems.Initialize();
+		}
 
-        private async void OnTurnChanged(Entity entity)
-        {
-            await _systems.Update();
-        }
-    }
+		private async void OnTurnChanged(Entity entity)
+		{
+			await _systems.Update();
+		}
+	}
 }
